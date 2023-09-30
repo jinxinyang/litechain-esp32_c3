5242d6b442ffa1311aa08092fe38fed8f10a3f90
jinxinyang <jinxinyang@chengtech.com>
2023-09-30 14:11:05 +0800

    完成基本的esp32访问openai接口的例程

--- main/http_request_example_main.c
+++ main/http_request_example_main.c
@@ -6,7 +6,6 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -16,14 +15,15 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
+
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
-#include "esp_timer.h"
 #include "sdkconfig.h"
-#include "../Plugins_http.h"
+#include "../litechain/MODULE/MODULE_CFG.h"
+#include <stdio.h>
 
 static const char *TAG = "example";
 
@@ -36,21 +36,7 @@ void app_main(void)
 
     ESP_ERROR_CHECK(example_connect());
 
-
-    char* url = "api1.t-chat.cn";
-    char* web_path = "/v1/chat/completions";
-    char* headers = "Content-Type: application/json\r\n"
-                    "Authorization: Bearer sk-q6iEheCNMGxHlhQd04702713Ba7143A28997D1C2F9A32a27\r\n";
-
-    char* data = "{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"system\", \"content\": "
-                     "\"You are a helpful assistant.\"}, {\"role\": \"user\", \"content\": \"Who is the president of the United States in 2016?\"}]}";
-    // 开始计时
-    uint64_t start_time = esp_timer_get_time();
-
-    ESP_LOGI(TAG, "... done reading from socket. Last read return=%s.", HTTP_Post(url,web_path,headers,data));
-    // 结束计时
-    uint64_t end_time = esp_timer_get_time();
-    // 计算运行时间（以微秒为单位）
-    uint64_t execution_time = end_time - start_time;
-    ESP_LOGI(TAG, "Execution time: %lld microseconds", execution_time);
+	LLM *llm;
+	llm = Openai_init("gpt-3.5-turbo-16k-0613",0.1,16*1024,"sk-q6iEheCNMGxHlhQd04702713Ba7143A28997D1C2F9A32a27","api1.t-chat.cn");
+	ESP_LOGI(TAG,"return data:%s",llm->run("/v1/chat/completions","Introduction to Wuhan?"));
 }
